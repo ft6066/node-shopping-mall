0a1367f655e623701cc9a477ff3216ac39ecd646
Seung Hwang <2018066@naver.com>
2025-08-01 21:06:37 +0900

    프론트엔드 추가

--- be/App.js
+++ be/App.js
@@ -3,11 +3,13 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
+const indexRouter = require("./routes/index");
 
 require("dotenv").config();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); // req.body가 객체로 인식이 됩니다.
+app.use("/api", indexRouter); // /api/user
 
 const mongoURI = process.env.LOCAL_DB_ADDRESS;
 mongoose
@@ -19,6 +21,6 @@ mongoose
     console.log("몽구스 연결 실패", err);
   });
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log("서버 켜짐");
+app.listen(process.env.PORT || 6500, () => {
+  console.log(process.env.PORT, "서버 켜짐");
 });
