ee1f3c9038be81ed4c6a76af14f07889b4fb5d01
Seung Hwang <2018066@naver.com>
2025-08-05 23:21:11 +0900

    어드민 페이지에서 상품 검색 및 페이지네이션 구현

--- be/controllers/product.controller.js
+++ be/controllers/product.controller.js
@@ -1,5 +1,6 @@
 const Product = require("../models/Product");
 
+const PAGE_SIZE = 1;
 const productController = {};
 productController.createProduct = async (req, res) => {
   try {
@@ -34,8 +35,24 @@ productController.createProduct = async (req, res) => {
 
 productController.getProducts = async (req, res) => {
   try {
-    const products = await Product.find({});
-    res.status(200).json({ status: "success", data: products });
+    const { page, name } = req.query;
+    const cond = name ? { name: { $regex: name, $options: "i" } } : {};
+    let query = Product.find(cond);
+    let response = { status: "success" };
+    if (page) {
+      query.skip((page - 1) * PAGE_SIZE).limit(PAGE_SIZE);
+      // 최종 몇개 페이지
+      // 데이터가 총 몇개 있는 지
+      const totalItemNum = await Product.countDocuments(cond);
+      // 데이터 총 개수 / PAGE_SIZE
+      const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
+      response.totalPageNum = totalPageNum;
+    }
+
+    const productList = await query.exec();
+    response.data = productList;
+
+    res.status(200).json(response);
   } catch (error) {
     res.status(400).json({ status: "fail", error: error.message });
   }
