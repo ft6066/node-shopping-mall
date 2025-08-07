235b265242c4378dbb6a2b98a2a13870eb2bce7b
Seung Hwang <2018066@naver.com>
2025-08-07 23:07:37 +0900

    cart.pi.js 라우트에 경로 설정을 잘못해서 다시 수정함

--- be/routes/cart.api.js
+++ be/routes/cart.api.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const cartController = require("../controllers/cart.controller");
 
 //로그인한 유저만 카트에 아이템을 추가할 수 있도록 토큰이 있는 지 확인
-router.post("/cart", authController.authenticate, cartController.addItemToCart);
+router.post("/", authController.authenticate, cartController.addItemToCart);
 
 module.exports = router;
